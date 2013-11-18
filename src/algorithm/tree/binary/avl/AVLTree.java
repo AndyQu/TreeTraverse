fb91204a97bda20efa40ffa93eb71fcc7f144ab0
AndyQu <717866228@qq.com>
2013-11-18 22:54:02 +0800

    Complete AVL insert

--- src/algorithm/tree/binary/avl/AVLTree.java
+++ src/algorithm/tree/binary/avl/AVLTree.java
@@ -52,13 +52,13 @@ public class AVLTree<E extends Comparable<E>> implements BSearchTree<E> {
         AvlNode<E> parent=(AvlNode<E>) child.getParent();
         boolean downIsLeft=(child.getLeft()==newAdded);
         boolean upIsLeft=(parent.getLeft()==child);
-        boolean needBalance=false;
         while(parent!=null){
             if(parent.isBalanced()){
                 int oh=parent.getHeight();
                 parent.updateHeight();
                 if(oh==parent.getHeight()){
-                    break;
+                	//stop updating height, because parent'height is not changed
+                    return;
                 }else{
                     downIsLeft=upIsLeft;
                     child=parent;
@@ -66,11 +66,98 @@ public class AVLTree<E extends Comparable<E>> implements BSearchTree<E> {
                     upIsLeft=(parent.getLeft()==child);
                 }
             }else{
-                needBalance=true;
+                break;
             }
         }
-        if(!needBalance){
-            
+        if(parent==null){
+            return;
+        }
+        
+        //balance the tree
+        //TODO
+        //Don't forget to link the rotated sub tree with its parent
+        AvlNode<E> father=(AvlNode<E>) parent.getParent();
+        AvlNode<E>	replaceForParent=null;
+        boolean isLeft=true;
+        if(father!=null){
+        	isLeft=(father.getLeft()==parent);
+        }
+        if(upIsLeft && downIsLeft){//left,left
+        	AvlNode<E> middle=(AvlNode<E>) child.getRight();
+        	
+        	child.setRight(parent);
+        	child.setParent(null);
+        	
+        	parent.setLeft(middle);
+        	parent.setParent(child);
+        	
+        	child.updateHeight();
+        	parent.updateHeight();
+        	
+        	replaceForParent=child;
+        }else if(!upIsLeft && !downIsLeft){//right,right
+        	AvlNode<E>middle=(AvlNode<E>) child.getLeft();
+        	
+        	child.setLeft(parent);
+        	child.setParent(null);
+        	
+        	parent.setRight(middle);
+        	parent.setParent(child);
+        	
+        	child.updateHeight();
+        	parent.updateHeight();
+        	
+        	replaceForParent=child;
+        }else if(upIsLeft && !downIsLeft){//left,right
+        	AvlNode<E>targetRoot=(AvlNode<E>) child.getRight();
+        	AvlNode<E>middle_left=(AvlNode<E>) targetRoot.getLeft();
+        	AvlNode<E>middle_right=(AvlNode<E>) targetRoot.getRight();
+        	
+        	child.setRight(middle_left);
+        	child.setParent(targetRoot);
+        	
+        	parent.setLeft(middle_right);
+        	parent.setParent(targetRoot);
+        	
+        	targetRoot.setLeft(child);
+        	targetRoot.setRight(parent);
+        	
+        	child.updateHeight();
+        	targetRoot.updateHeight();
+        	parent.updateHeight();
+        	
+        	replaceForParent=targetRoot;
+        }else{//right,left
+        	AvlNode<E>targetRoot=(AvlNode<E>) child.getLeft();
+        	AvlNode<E>middle_left=(AvlNode<E>) targetRoot.getLeft();
+        	AvlNode<E>middle_right=(AvlNode<E>) targetRoot.getRight();
+        	
+        	child.setLeft(middle_right);
+        	child.setParent(targetRoot);
+        	
+        	parent.setRight(middle_left);
+        	parent.setParent(targetRoot);
+        	
+        	targetRoot.setLeft(parent);
+        	targetRoot.setRight(child);
+        	
+        	child.updateHeight();
+        	targetRoot.updateHeight();
+        	parent.updateHeight();
+        	
+        	replaceForParent=targetRoot;
+        }
+        
+        //link the rotated tree with father
+        if(father==null){
+        	root=replaceForParent;
+        	replaceForParent.setParent(null);
+        }else if(isLeft){
+        	father.setLeft(replaceForParent);
+        	replaceForParent.setParent(father);
+        }else{
+        	father.setRight(replaceForParent);
+        	replaceForParent.setParent(father);
         }
     }
 
@@ -82,8 +169,18 @@ public class AVLTree<E extends Comparable<E>> implements BSearchTree<E> {
 
     @Override
     public BSearchNode<E> search(E v) {
-        // TODO Auto-generated method stub
-        return null;
+        BSearchNode<E>cursor=root;
+        while(cursor!=null){
+        	int r=cursor.getValue().compareTo(v);
+        	if(r==0){
+        		break;
+        	}else if(r>0){
+        		cursor=(BSearchNode<E>) cursor.getLeft();
+        	}else{
+        		cursor=(BSearchNode<E>) cursor.getRight();
+        	}
+        }
+        return cursor;
     }
 
 }
