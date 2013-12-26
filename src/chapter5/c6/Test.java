9b719893a687dcf0d0df8f486544bd67de294d33
Guocheng Qu <quguocheng@qingtingfm.com>
2013-12-26 17:32:06 +0800

    add some test cases for SkyLine

--- src/chapter5/c6/Test.java
+++ src/chapter5/c6/Test.java
@@ -9,7 +9,20 @@ public class Test {
         SkyLine skyD=new SkyLine(12,7,16);
         SkyLine skyE=new SkyLine(14,3,25);
         SkyLine skyF=new SkyLine(19,18,22);
+        
         SkyLine skyG=new SkyLine(23,13,29);
+        SkyLine skyJ=new SkyLine(29,13,50);
+        SkyLine skyH=new SkyLine(29,20,50);
+        
+        //not overlaped
+        SkyLine.merge(skyD,skyA).show();
+        
+        //connected
+        SkyLine.merge(skyG,skyJ).show();
+        SkyLine.merge(skyG,skyH).show();
+        
+        //covered
+        SkyLine.merge(skyE,skyF).show();
         
         SkyLine.merge(skyA,skyB,skyC,skyD,skyE,skyF,skyG).show();
     }
